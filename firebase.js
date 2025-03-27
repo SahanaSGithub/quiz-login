5f9d9d12e799feb43ef5ff61866403ec8896da3a
SahanaSGithub <153353761+SahanaSGithub@users.noreply.github.com>
2025-03-27 09:10:50 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBU2S2vQLhdc4iuEwQO5sp2hIVHjOeWRJI",
+  authDomain: "quiz-a2dbb.firebaseapp.com",
+  projectId: "quiz-a2dbb",
+  storageBucket: "quiz-a2dbb.firebasestorage.app",
+  messagingSenderId: "1063818883651",
+  appId: "1:1063818883651:web:45c685d6cede292c039af6",
+  measurementId: "G-7QKW2SY7F7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
